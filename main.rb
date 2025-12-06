d038d00bfd30ecc3d94219e611ccb5fa65296d95
Kostas Kolyvas <kkolyvas@navarino.gr>
2025-12-06 15:03:51 +0200

    Allow unstripped input parsing

--- main.rb
+++ main.rb
@@ -12,13 +12,15 @@ end
 opts = {
   part: 1,
   day: Date.today.day,
-  test: false
+  test: false,
+  strip: true
 }
 
 OptionParser.new do |opt|
   opt.on('--part PART', '-p PART') { |o| opts[:part] = o.to_i }
   opt.on('--day DAY', '-d DAY') { |o| opts[:day] = o.to_i }
   opt.on('--test', '-t') { opts[:test] = true }
+  opt.on('--no-strip', '-n') { opts[:strip] = false }
 end.parse!
 
 raise 'Day not released yet!' if Date.new(Date.today.year, Date.today.month, opts[:day]) > Date.today
@@ -26,4 +28,4 @@ raise 'Part must be 1 or 2' if opts[:part] != 1 && opts[:part] != 2
 
 day_class = Object.const_get("Days::D#{opts[:day]}")
 
-puts day_class.solve(opts[:part], Utils.load(opts[:day], test: opts[:test]))
+puts day_class.solve(opts[:part], Utils.load(opts[:day], test: opts[:test], strip: opts[:strip]))
--- utils.rb
+++ utils.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 module Utils
-  def self.load(day, test: false)
+  def self.load(day, test: false, strip: false)
     filename = "input/d#{day}#{test ? '_test' : ''}.txt"
-    File.readlines(filename).reject do |line|
-      line.start_with?('#')
-    end.map(&:strip)
+
+    lines =
+      File.readlines(filename).reject do |line|
+        line.start_with?('#')
+      end
+
+    lines.map!(&:strip) if strip
+    lines.map { |l| l.chomp("\n") }
   end
 
   def self.as_grid(lines)
